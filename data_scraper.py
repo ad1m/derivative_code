26b6147881190fae60299d85113b2e4d39b9920a
ad1m <alieberman3@mail.gatech.edu>
2017-01-02 16:25:34 -0500

    Add lesson 1: data_scraper

---
+++ data_scraper.py
@@ -0,0 +1,27 @@
+import pandas_datareader.data as web
+import os
+import time
+
+def download_data(tickers,start='all',end='all',metric=False,all_data=False):
+    if all_data==True:
+        end = time.strftime("%x")
+        start = '01/01/1970'
+
+    directory = 'stock_data'
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+    d = {}
+    for ticker in tickers:
+        filename = directory+'/'+ticker+'.csv'
+        d[ticker] = web.DataReader(ticker,"yahoo",start,end)
+        d[ticker].to_csv(filename)
+        return
+
+if __name__ == '__main__':
+    tickers = ['AAPL','BAC','GILD']
+    start = '2016-01-01'
+    end = '2016-12-21'
+    end = 'today'
+    metric='Adj Close' #this will give us open, high, low, close, volume, Adj Close
+    download_data(tickers,start,end,metric)
